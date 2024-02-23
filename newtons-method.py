e250bc40fb2fa4bf3e5b76126ac06a41ff22bd79
Artemiy <andrikv@gmail.com>
2024-02-23 19:19:07 +0300

    fix: fixed secant method infinity execute in some cases and added newston_simple method

--- newtons-method.py
+++ newtons-method.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# utils
+
 def stop_criterion(x_n1, x_n):
     numerator = x_n1 - x_n
     denominator = 1 - ((x_n1 - x_n) / (x_n - x_n1))
@@ -16,11 +18,8 @@ def f(x, arr):
         sum += x**i * arr[i - 1]
     
     return sum
-     
 
-arr = [-6, 11, -6, 1] # (x-1) * (x-2) * (x-3), the roots of equations x = {1, 2, 3}
-x_0 = 10
-
+# solve methods
 
 # Newton's method with Broyden's 
 # By default c = 1 and using default Newton method, but you can set c and it will be use Broyden's
@@ -36,20 +35,46 @@ def newton(f, x_0, eps = 10 ** (-2), c = 1):
         
     return x[1]
 
-def secant(f, x_0, eps = 10**(-3)):
+# Newton's simplify method
+def newton_simple(f, x_0, eps = 10 ** (-2)):
     x_1 = x_0 - f(x_0, arr) / f(x_0, deriv(arr))
     x = [x_0, x_1] 
-    
-    while stop_criterion(x_1, x_0) > eps:
-        temp = x[1] - f(x[1], arr) * (x[1] - x[0]) / (f(x[1], arr) - f(x[0], arr)) 
-        x[0] = x[1]
-        x[1] = temp
-
+      
+    # Default (weak criterion): while abs(x[1] - x[0]) >= eps:
+    while stop_criterion(x[1], x[0]) >= eps: 
+        temp = x[1] 
+        x[1] = x[1] - f(x[1], arr) / f(x_0, deriv(arr)) 
+        x[0] = temp
+        
     return x[1]
 
-print(newton(f, x_0, 10**(-5)))    # -> 3.0000000000000036
-print(newton(f, x_0, 10**(-5), 3)) # -> 1.9999880265095271
+# Secant method
+def secant(f, x_0, eps = 10**(-3)):
+    x_1 = x_0 - f(x_0, arr) / f(x_0, deriv(arr))
+    x = [x_0, x_1]
+    
+    while stop_criterion(x[1], x[0]) > eps:
+        curr = x[1]
+        x[1] = x[0] - (f(x[0], arr) * (curr - x[0])) / (f(curr, arr) - f(x[0], arr))
+        x[0] = curr 
 
+    return x[-1]
 
-print(secant(f, x_0, 10**(-5)))    # -> 3.0000000000000036
-print(secant(f, x_0, 10**(-5)))    # -> 1.9999880265095271
+# Vars
+
+arr = [-6, 11, -6, 1] # (x-1) * (x-2) * (x-3), the roots of equations x = {1, 2, 3}
+x_0 = 10
+
+# Usage
+
+# Newton default
+print(newton(f, x_0, 10**(-5)))        # -> 3.0000000000000036
+
+# Newton with Broyden
+print(newton(f, x_0, 10**(-5), 3))     # -> 1.9999880265095271
+
+# Newton simplify
+print(newton_simple(f, x_0, 10**(-5))) # -> 3.0062591799208933
+
+# Secant 
+print(secant(f, x_0, 10**(-5)))        # -> 3.0000002668821484
