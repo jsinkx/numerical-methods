97dde6796a89485be1541cb4b34e37c1df0eb550
Artemiy <andrikv@gmail.com>
2024-04-14 21:45:03 +0300

    feat: created direct-deployment-and-iterations-method.py

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Numerical methods
 
+### Methods for calculating the roots of the equation
+
 -  Practical lesson 13.02.24 - **dichotomy method** - [`dichotomy-method.py`](./dichotomy-method.py)
 
 -  Practical lesson 20.02.24 - **newtons methods & secant method** - [`newtons-method.py`](./newtons-method.py)
@@ -9,4 +11,14 @@
 
 -  Practical lesson 05.03.24 - **chord method** - [`chord-method.py`](./chord-method.py)
 
+---
+
+### Methods for calculating SLE
+
 -  Practical lesson 12.03.24 - **SLE Gauss's 3 methods** - [`SLE-gauss-methods.py`](./SLE-gauss-methods.py)
+
+---
+
+### Methods for calculating eigen values & vectors
+
+-  Practical lesson 09.04.24 - **Direct deployment & iteration methods** - [`direct-deployment-and-iterations-method.py`](./direct-deployment-and-iterations-method.py)
---
+++ direct-deployment-and-iterations-method.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def stop_criterion(x_1, x_0, eps=1e-3):
+    return np.abs(x_1 - x_0) < eps
+
+# Direct deployment method
+
+# Iterations method
+
+def iterations_method(A, eps=1e-3):
+    x_0 = np.ones(len(A))
+    x_1 = np.dot(A, x_0)
+    
+    lambda_0 = x_1[0] / x_0[0]
+    lambda_1 = lambda_0
+    
+    i = 0
+    
+    while True:
+        i += 1
+        
+        x_0 = x_1.copy()
+        x_1 = np.dot(A, x_0)
+        
+        lambda_0 = lambda_1
+        lambda_1 = x_1[0] / x_0[0]
+            
+        # Normalization eigen vector on each 5 step    
+        if i % 5 == 0:
+            x_1 /= np.linalg.norm(x_1)
+        
+        if stop_criterion(lambda_1, lambda_0, eps):
+            break
+    
+    # Normalization eigen vector in end
+    x_1 /= np.linalg.norm(x_1)
+    
+    return x_1, lambda_1
+
+A_2 = np.array([[5, 1, 2], [1, 4, 1], [2, 1, 3]])
+
+A_2_eigen_vector, A_2_eigen_value = iterations_method(A_1)
+
+print("Eigen vector for A_2 matrix:", A_2_eigen_vector) # -> [0.75249496 0.4318604  0.49724031]
+print("Eigen value, for A_2 matrix:" , A_2_eigen_value) # -> 6.8958704371104655
+
